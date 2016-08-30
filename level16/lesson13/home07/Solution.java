ab41ebfc155e527c7609412f3d077892830807b4
Vladimir Pligos <v.pligos@globus-ltd.com>
2016-08-30 22:58:38 +0300

    lvl16 lesson13 home7-10

--- level16/lesson13/home07/Solution.java
+++ level16/lesson13/home07/Solution.java
@@ -66,6 +66,16 @@ public class Solution {
         @Override
         public void run() {
             //Add your code here - добавь код тут
+            try {
+                for (int i = 0; i <OnlineGame.steps.size() ; i++) {
+                    System.out.println(getName() + ":" + OnlineGame.steps.get(i));
+                    Thread.sleep(1000 / rating);
+                }
+                System.out.println(getName()+":победитель!" );
+                OnlineGame.isWinnerFound=true;
+            } catch (InterruptedException e) {
+                System.out.println(getName() + ":проиграл");
+            }
         }
     }
 }
