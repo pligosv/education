c3e232a3a878e31cbc527da5199d82f089b4c740
Vladimir Pligos <v.pligos@globus-ltd.com>
2016-09-20 22:46:51 +0300

    no message

---
+++ level19/lesson05/task01/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level19.lesson05.task01;
+
+/* Четные байты
+Считать с консоли 2 имени файла.
+Вывести во второй файл все байты с четным индексом.
+Пример: второй байт, четвертый байт, шестой байт и т.д.
+Закрыть потоки ввода-вывода.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
